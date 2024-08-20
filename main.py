48bea06845643e4aeee9fd61ffc785d28e9d87dd
Carlo Yannuzzi <carlogyannuzzi@gmail.com>
2024-08-20 18:45:29 -0400

    defined a player class, main game loop.

---
+++ main.py
@@ -0,0 +1,27 @@
+from player import Player
+import pygame
+from constants import *
+
+def main():
+    print("Starting asteroids!")
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    time_clock = pygame.time.Clock()
+    dt = 0
+    player = Player(x=SCREEN_WIDTH/2, y=SCREEN_HEIGHT/2)
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        screen.fill("black")
+        player.draw(screen)
+        pygame.display.flip()
+        dt =  time_clock.tick(60) / 1000
+
+
+
+
+
+
+if __name__ == "__main__":
+    main()
